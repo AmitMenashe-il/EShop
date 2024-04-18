1087e8c1fd6ac18d65a463c65fed90bd78849fc2
Amit Menashe <127229082+AmitMenashe-il@users.noreply.github.com>
2024-04-19 00:58:30 +0300

    Update test_settings.py

--- tests/api/test_settings.py
+++ tests/api/test_settings.py
@@ -9,10 +9,10 @@ def test_database_name_in_settings():
     with open(settings_file_path, 'r') as f:
         settings_contents = f.read()
 
-    assert "BASE_DIR / 'db.sqlite3'" not in settings_contents
+    assert "BASE_DIR / 'db.sqlite3'" in settings_contents
 
 def test_allowed_hosts():
-    assert '*' not in settings.ALLOWED_HOSTS
+    assert '*' in settings.ALLOWED_HOSTS
 
 def test_language_code():
     assert settings.LANGUAGE_CODE == 'en-us'
