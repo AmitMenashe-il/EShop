ca2130d32c64f5a3ec2697173b1186151dee2b21
Amit Menashe <127229082+AmitMenashe-il@users.noreply.github.com>
2024-04-19 00:50:57 +0300

    Update test_settings.py

--- tests/api/test_settings.py
+++ tests/api/test_settings.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 
-def test_database_name():
-    assert settings.DATABASES['default']['NAME'] == str(settings.BASE_DIR / 'db.sqlite3')
+def test_database_name_in_settings():
+    settings_file_path = os.path.join(settings.BASE_DIR, 'backend', 'settings.py')
+
+    with open(settings_file_path, 'r') as f:
+        settings_contents = f.read()
+
+    assert "BASE_DIR / 'db.sqlite3'" not in settings_contents
 
 def test_allowed_hosts():
-    assert settings.ALLOWED_HOSTS == ['*']
+    assert '*' not in settings.ALLOWED_HOSTS
 
 def test_language_code():
     assert settings.LANGUAGE_CODE == 'en-us'
