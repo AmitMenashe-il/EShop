084698289e6aa7b6a84e021ef643b74040d155c2
Amit Menashe <127229082+AmitMenashe-il@users.noreply.github.com>
2024-04-20 14:40:59 +0300

    Update test_settings.py

--- tests/api/test_settings.py
+++ tests/api/test_settings.py
@@ -4,10 +4,7 @@ from django.test import SimpleTestCase
 
 
 def test_database_name_in_settings():
-    settings_file_path = os.path.join(settings.BASE_DIR, 'backend', 'settings.py')
-    with open(settings_file_path, 'r') as f:
-        settings_contents = f.read()
-    assert "BASE_DIR / 'db.sqlite3'" in settings_contents
+    assert settings.DATABASES['default']['NAME'] == BASE_DIR / 'db.sqlite3'
 
 def test_allowed_hosts():
     assert '*' in settings.ALLOWED_HOSTS
