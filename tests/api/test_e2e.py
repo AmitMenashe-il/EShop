3b3846f3d76b9265b09dd4f02d247be7094c354d
Amit Menashe <127229082+AmitMenashe-il@users.noreply.github.com>
2024-04-20 11:09:32 +0300

    Update test_e2e.py

--- tests/api/test_e2e.py
+++ tests/api/test_e2e.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as chrome_service
 
+
+driver_path=ChromeDriverManager().install()
+driver_service=chrome_service(chrome_driver_path)
 
 def test_page_load():
   # set chrome headless, no sandbox and no shared memory
@@ -10,7 +15,7 @@ def test_page_load():
     chrome_options.add_argument("--no-sandbox")  
     chrome_options.add_argument("--disable-dev-shm-usage") 
 
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(service=driver_server, options=chrome_options)
 
     try:
         driver.get("https://localhost:5000")
