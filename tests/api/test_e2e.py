a06b4622694b109c02af198714d9b356b72cde14
Amit Menashe <127229082+AmitMenashe-il@users.noreply.github.com>
2024-04-20 17:15:11 +0300

    Update test_e2e.py

--- tests/api/test_e2e.py
+++ tests/api/test_e2e.py
@@ -20,7 +20,7 @@ driver = webdriver.Chrome(service=driver_service, options=chrome_options)
 
 def check_title():
    try:
-      driver.get("https://localhost:5000")
+      driver.get("http://localhost:5000")
 
       #assert "Amit's Project EShop" in driver.title  # Check page title
       assert "mushmush" in driver.title  # Check page title
